14338894dc445d3a13cbca569ca804f3fb8b394a
carranza12 <franckgta3@gmail.com>
2024-06-01 19:23:21 -0600

    conexion de mongoose y nest js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { env } from 'process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
